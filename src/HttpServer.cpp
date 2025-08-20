9bcd7ee2cde1e56dbdd50f3cf58f5e75d3d6f955
Tom SEITZ <tom.seitz@viacesi.fr>
2025-08-20 10:22:07 +0200

    added header file for HttpServer class

--- src/HttpServer.cpp
+++ src/HttpServer.cpp
@@ -1,37 +1,22 @@
-#include "cpprest/asyncrt_utils.h"
 #include "cpprest/http_listener.h"
-#include "cpprest/json.h"
-#include "cpprest/uri.h"
-#include <algorithm>
-#include <fstream>
+// #include "cpprest/json.h"
+// #include "cpprest/uri.h"
+// #include <algorithm>
+// #include <fstream>
 #include <iostream>
-#include <random>
-#include <sstream>
-#include <string>
-#include <vector>
+// #include <random>
+// #include <sstream>
+// #include <string>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 using namespace web;
 using namespace http;
-using namespace utility;
-using namespace http::experimental::listener;
+// using namespace utility;
+// using namespace http::experimental::listener;
 
-class HttpServer
-{
-private:
-    void handle_get(http_request message);
-    void handle_put(http_request message);
-    void handle_post(http_request message);
-    void handle_delete(http_request message);
-    void handle_error(pplx::task<void>& t);
-    http_listener m_listener;
+#include "HttpServer.h"
 
-public:
-    HttpServer(utility::string_t url);
-    virtual ~HttpServer();
-    pplx::task<void>open(){return m_listener.open();}
-    pplx::task<void>close(){return m_listener.close();}
-};
 
 
 // public methods
@@ -63,9 +48,9 @@ void HttpServer::handle_error(pplx::task<void>& t)
 
 void HttpServer::handle_get(http_request message)
 {
-    ucout << U("Get request has been received.") << endl;
-    ucout << U("Detail:") << endl;
-    ucout <<  message.to_string() << endl;
+    ucout << U("Get request has been received.") << std::endl;
+    ucout << U("Detail:") << std::endl;
+    ucout <<  message.to_string() << std::endl;
     
     // useless line
     std::vector<utility::string_t> paths = http::uri::split_path(http::uri::decode(message.relative_uri().path()));
@@ -77,9 +62,9 @@ void HttpServer::handle_get(http_request message)
 
 void HttpServer::handle_post(http_request message)
 {
-    ucout << U("Post request has been received.") << endl;
-    ucout << U("Detail:") << endl;
-    ucout <<  message.to_string() << endl;
+    ucout << U("Post request has been received.") << std::endl;
+    ucout << U("Detail:") << std::endl;
+    ucout <<  message.to_string() << std::endl;
     
 
     message.reply(status_codes::OK, U("TEMP JSON POST"));
@@ -89,9 +74,9 @@ void HttpServer::handle_post(http_request message)
 
 void HttpServer::handle_put(http_request message)
 {
-    ucout << U("Put request has been received.") << endl;
-    ucout << U("Detail:") << endl;
-    ucout <<  message.to_string() << endl;
+    ucout << U("Put request has been received.") << std::endl;
+    ucout << U("Detail:") << std::endl;
+    ucout <<  message.to_string() << std::endl;
     
 
     message.reply(status_codes::OK, U("TEMP JSON PUT"));
@@ -101,9 +86,9 @@ void HttpServer::handle_put(http_request message)
 
 void HttpServer::handle_delete(http_request message)
 {
-    ucout << U("Delete request has been received.") << endl;
-    ucout << U("Detail:") << endl;
-    ucout <<  message.to_string() << endl;
+    ucout << U("Delete request has been received.") << std::endl;
+    ucout << U("Detail:") << std::endl;
+    ucout <<  message.to_string() << std::endl;
     
 
     message.reply(status_codes::OK, U("TEMP JSON DELETE"));
---
+++ src/HttpServer.h
@@ -0,0 +1,22 @@
+#include "cpprest/http_listener.h"
+
+using namespace web;
+using namespace http;
+using namespace http::experimental::listener;
+
+class HttpServer
+{
+private:
+    void handle_get(http_request message);
+    void handle_put(http_request message);
+    void handle_post(http_request message);
+    void handle_delete(http_request message);
+    void handle_error(pplx::task<void>& t);
+    http_listener m_listener;
+
+public:
+    HttpServer(utility::string_t url);
+    virtual ~HttpServer();
+    pplx::task<void>open(){return m_listener.open();}
+    pplx::task<void>close(){return m_listener.close();}
+};
